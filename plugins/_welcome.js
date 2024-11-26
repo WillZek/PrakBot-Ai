fc84a8e2811491b53e158578a6e4bfe8407b5a72
Niño Piña <ninopina10@gmail.com>
2024-11-26 14:50:58 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,8 +3,8 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://i.ibb.co/SJKqCQL/file.jpg')
-  let img = await (await fetch(`${pp}`)).buffer()
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://iili.io/dtLx2GR.md.jpg')
+  let img = 'https://i.ibb.co/SJKqCQL/file.jpg')).buffer()
   let chat = global.db.data.chats[m.chat]
 
   if (chat.bienvenida && m.messageStubType == 27) {
