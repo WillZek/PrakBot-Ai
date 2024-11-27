3639bdd93cc60176cb3547b38108b3a3f72a163a
Niño Piña <ninopina10@gmail.com>
2024-11-27 08:55:19 -0600

    Actualizar tourl-generarimg3.js

--- plugins/tourl-generarimg3.js
+++ plugins/tourl-generarimg3.js
@@ -8,7 +8,7 @@ m.react('🕒');
 await conn.sendMessage(m.chat, {text: '*🧑‍💻 Espere, Estamos Trabajando en su imagen*'}, {quoted: m});
 try {
 // Hacemos la solicitud a la nueva API con la clave API
-const apiKey = 'tu_api_key'; // Reemplaza con tu clave API real
+const apiKey = 'api_endpoint_here'; // Reemplaza con tu clave API real
 const response = await fetch(`https://api.dynapictures.com/designs/${apiKey}`);
 // Verificamos si la respuesta fue exitosa
 if (!response.ok) throw new Error('Network response was not ok');
