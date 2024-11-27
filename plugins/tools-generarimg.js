64b226a09c1dd42f63227d9ed987bae8ea1265ab
Niño Piña <ninopina10@gmail.com>
2024-11-26 22:19:25 -0600

    Actualizar tools-generarimg.js

--- plugins/tools-generarimg.js
+++ plugins/tools-generarimg.js
@@ -1,17 +1,17 @@
 import fetch from 'node-fetch';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) throw `*💛 ingrese una petición para generar una imagen con el texto...*`;
+if (!text) throw `*👻 ingrese un texto para generar poder generar su imagen*`;
 m.react('🕒');
-await conn.sendMessage(m.chat, {text: '*💛 Espere un momento...*'}, {quoted: m});
+await conn.sendMessage(m.chat, {text: '*👻 Generando Imagen*'}, {quoted: m});
 try {
 const response = await fetch(`https://eliasar-yt-api.vercel.app/editar-imagen?texto=${encodeURIComponent(text)}`);
 if (!response.ok) throw new Error('Network response was not ok');
 const buffer = await response.buffer();
-m.react('☑️');
+m.react('✔️');
 await conn.sendMessage(m.chat, {image: buffer}, {quoted: m});
 } catch (error) {
 console.error(error);
-throw `*🚨 Ocurrió un error al generar la imagen...*`;
+throw `*🚨 Lo Sentimos, ha ocurrido un error 😔*`;
 }
 }
 handler.tags = ['tools'];
