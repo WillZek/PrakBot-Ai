05eba92d165e2503d3dd9f11372cdd11cbea5071
Niño Piña <ninopina10@gmail.com>
2024-11-28 09:10:57 -0600

    Actualizar tools-generarimg.js

--- plugins/tools-generarimg.js
+++ plugins/tools-generarimg.js
@@ -1,24 +1,25 @@
-// Codigo Hecho Por Niño Piña wa.me/50557865603
-// Api Creada Por EliasarYT
-
+// Código Creado Por Niño Piña Wa.me/50557865603
 import fetch from 'node-fetch';
-const handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*👻 ingrese un texto para generar poder generar su imagen*`;
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+// Dividir el texto en partes para obtener el nombre del logo y la URL
+const args = text.split(' ');
+const logoText = args[0]; // Nombre del logo
+const imageUrl = args[1]; // URL de la imagen
+if (!logoText || !imageUrl) throw `*👻 ingrese el nombre del logo y la URL de la imagen*`;
 m.react('🕒');
-await conn.sendMessage(m.chat, { text: '*👻 Generando Imagen*' }, { quoted: m });
+await conn.sendMessage(m.chat, {text: '*👻 Generando Imagen*'}, {quoted: m});
 try {
-const ImageUrl = 'https://example.com/image.jpg'; // Reemplaza con la URL de la imagen que deseas utilizar
-const response = await fetch(`https://eliasar-yt-api.vercel.app/editar-imagen?texto=${encodeURIComponent(text)}&url=${encodeURIComponent(ImageUrl)}`);
+const response = await fetch(`https://eliasar-yt-api.vercel.app/editar-imagen?texto=${encodeURIComponent(logoText)}&url=${encodeURIComponent(imageUrl)}`);
 if (!response.ok) throw new Error('Network response was not ok');
 const buffer = await response.buffer();
 m.react('✔️');
-await conn.sendMessage(m.chat, { image: buffer }, { quoted: m });
+await conn.sendMessage(m.chat, {image: buffer}, {quoted: m});
 } catch (error) {
 console.error(error);
 throw `*🚨 Lo Sentimos, ha ocurrido un error 😔*`;
 }
 }
 handler.tags = ['tools'];
-handler.help = ['generarimg'];
-handler.command = ['generarimg', 'img'];
+handler.help = ['genearimg'];
+handler.command = ['genearimg', 'imgg'];
 export default handler;
