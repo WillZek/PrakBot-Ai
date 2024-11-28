14d1336237c3cee0a3f854866240e808ad6aa1a4
Niño Piña <ninopina10@gmail.com>
2024-11-28 08:58:28 -0600

    Actualizar tools-generarimg.js

--- plugins/tools-generarimg.js
+++ plugins/tools-generarimg.js
@@ -1,12 +1,14 @@
 // Código Creado Por Niño Piña Wa.me/50557865603
-
 import fetch from 'node-fetch';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) throw `*👻 ingrese un texto para generar poder generar su imagen*`;
+const args = text.split('|'); // Separa el texto en dos partes: nombre y URL
+if (args.length < 2) throw `*👻 ingrese el nombre del logo y el enlace de la imagen en el formato: Nombre|URL*`;
+const logoName = args[0].trim(); // Nombre del logo
+const imageUrl = args[1].trim(); // URL de la imagen
 m.react('🕒');
 await conn.sendMessage(m.chat, {text: '*👻 Generando Imagen*'}, {quoted: m});
 try {
-const response = await fetch(`https://eliasar-yt-api.vercel.app/editar-imagen?texto=${encodeURIComponent(text)}`);
+const response = await fetch(`https://eliasar-yt-api.vercel.app/editar-imagen?texto=${encodeURIComponent(logoName)}&url=${encodeURIComponent(imageUrl)}`);
 if (!response.ok) throw new Error('Network response was not ok');
 const buffer = await response.buffer();
 m.react('✔️');
