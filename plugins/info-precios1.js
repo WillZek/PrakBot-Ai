c9deb02891555afc316fa3bac8cbdc1a60ff3f37
Niño Piña <ninopina10@gmail.com>
2024-12-03 13:00:38 -0600

    Actualizar info-precios1.js

--- plugins/info-precios1.js
+++ plugins/info-precios1.js
@@ -31,6 +31,6 @@ conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: str }, { quoted: m
 }
 handler.help = ['precios1'];
 handler.tags = ['info'];
-handler.command = ['precios1'];
+handler.command = ['precios1', 'prec1'];
 handler.group = true;
 export default handler;
