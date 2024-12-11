f7a2b0a4e2df7653e0a8e47adb5c0291b7dea627
Prakharper <158788969+Prakharper@users.noreply.github.com>
2024-12-11 12:19:45 -0400

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,10 +1,10 @@
 import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
-await m.react('🕓')
+await m.react('🤖')
 if (conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 await conn.reply(m.chat, stdout.toString(), m)
-await m.react('✅')
+await m.react('🚀')
 }}
 handler.help = ['update']
 handler.tags = ['owner']
