ea1a06e928018987be25ad5a228e1cabf908dfb9
Niño Piña <ninopina10@gmail.com>
2024-12-15 13:50:43 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -201,7 +201,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let pp14 = 'https://i.ibb.co/K6KDND7/file.jpg'
     let pp15 = 'https://i.ibb.co/JKK2gQG/file.jpg'
     let img = 'https://i.ibb.co/f986Hnr/file.jpg'
-    await m.react('🚀')
+    await m.react('⚔️')
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
    //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo)
