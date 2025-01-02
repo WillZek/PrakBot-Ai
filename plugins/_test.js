286368544eaf79c057b8351adc8ca5ad9678469a
Niño Piña <ninopina10@gmail.com>
2025-01-01 21:53:38 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,21 +1,26 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-let txt = `Nose?`
-conn.sendMessage(m.chat, { text: txt, caption: "1234", footer: wm, buttons: [
-  {
-    buttonId: ".menu", 
-    buttonText: { 
-      displayText: 'test' 
-    }
-  }, {
-    buttonId: ".s", 
-    buttonText: {
-      displayText: "Hola"
-    }
-  }
-],
-  viewOnce: true,
-  headerType: 1,
-}, { quoted: m })
+  let txt = `Nose?`
+  conn.sendMessage(m.chat, {
+    text: txt,
+    caption: "1234",
+    footer: wm,
+    buttons: [
+      {
+        buttonId: ".menu",
+        buttonText: {
+          displayText: 'test'
+        }
+      },
+      {
+        buttonId: ".s",
+        buttonText: {
+          displayText: "Hola"
+        }
+      }
+    ],
+    viewOnce: true,
+    headerType: 1,
+  }, { quoted: m }) // Asegúrate de que este sea el tercer argumento
 }
 handler.command = ['test']
 export default handler
