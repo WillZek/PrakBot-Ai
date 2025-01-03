e68a1b26ca2a1da363ce979d438cb5f103fdd7d0
Niño Piña <ninopina10@gmail.com>
2025-01-03 11:26:16 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,26 +1,33 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  let txt = `Nose?`
-  conn.sendMessage(m.chat, {
-    text: txt,
-    caption: "1234",
-    footer: textbot,
-    buttons: [
-      {
-        buttonId: ".menu",
-        buttonText: {
-          displayText: 'test'
-        }
-      },
-      {
-        buttonId: ".s",
-        buttonText: {
-          displayText: "Hola"
-        }
+  try {
+    let banner = 'https://i.ibb.co/s9N9QhG/file.jpg';
+    let txt = `Nose?`;
+
+    conn.sendMessage(m.chat, { 
+      image: { url: banner }, 
+      caption: "nazi bot muaaajjjj", 
+      footer: "by nazi", 
+      buttons: [
+        { buttonId: "$ echo hola gei", buttonText: { displayText: "hola" } }, 
+        { buttonId: "$ echo adios gei", buttonText: { displayText: "bye" } }
+      ], 
+      headerType: 6, 
+      viewOnce: true, 
+      contextInfo: {
+        forwardedNewsletterMessageInfo: { 
+          newsletterJid: '120363382214376298@newsletter', 
+          serverMessageId: '', 
+          newsletterName: 'Test Bot' 
+        }, 
+        isForwarded: true
       }
-    ],
-    viewOnce: true,
-    headerType: 1,
-  }, { quoted: m }) // Asegúrate de que este sea el tercer argumento
+    }, { quoted: m });
+
+  } catch (error) {
+    m.reply(`Error: ${error.message}`);
+    m.react('✖️');
+  }
 }
-handler.command = ['test']
-export default handler
+
+handler.command = ['test'];
+export default handler;
